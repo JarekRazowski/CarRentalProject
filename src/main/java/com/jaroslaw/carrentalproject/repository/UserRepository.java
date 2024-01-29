18aff0597f05d6b79f14b9eabd3a02c76ee1b1f7
Jarosław Razowski <j.razowski19@onet.pl>
2024-01-29 18:40:05 +0100

    1

--- src/main/java/com/jaroslaw/carrentalproject/repository/UserRepository.java
+++ src/main/java/com/jaroslaw/carrentalproject/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.jaroslaw.carrentalproject.repository;
 
 import com.jaroslaw.carrentalproject.entity.User;
+import com.jaroslaw.carrentalproject.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findFirstByEmail(String email);
+    User findByUserRole(UserRole userRole);
 }
