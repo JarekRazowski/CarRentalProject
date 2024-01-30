4254ef197b4464e566d4738e1fea7ccfd0b56d04
Jarosław Razowski <j.razowski19@onet.pl>
2024-01-30 19:00:51 +0100

    1

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { PostCarComponent } from './components/post-car/post-car.component';
+import { NgZorroImportsModule } from 'src/app/NgZorroImportsModule';
 
 
 @NgModule({
@@ -13,7 +14,10 @@ import { PostCarComponent } from './components/post-car/post-car.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    NgZorroImportsModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class AdminModule { }
--- src/app/modules/admin/components/post-car/post-car.component.ts
+++ src/app/modules/admin/components/post-car/post-car.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-car',
@@ -6,5 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-car.component.scss']
 })
 export class PostCarComponent {
+  
+  constructor(private fb: FormBuilder){}
+
+  postCarForm!: FormGroup;
+  isSpinning: boolean = false;
+  selectedFile!: File;
+  imagePreview!: string | ArrayBuffer | null;
+  listOfOption: Array<{ label: string; value: string }> = [];
+  listOfBrands = ["BMW", "AUDI", "FERRARI", "TESLA", "VOLVO", "TOYOTA", "HONDA", "FORD", "NISSAN", "HYUNDAI", "LEXUS", "KIA"];
+  listOfType = ["Petrol", "Hybrid", "Diesel", "Electric", "CNG"];
+  listOfColor = ["Red", "White", "Blue", "Orange", "Grey", "Silver"];
+  listOfTransmission = ["Manual", "Automatic"];
+
+  ngOnInit(){
+    this.postCarForm = this.fb.group({
+      name:[null, Validators.required],
+      brand:[null, Validators.required],
+      type:[null, Validators.required],
+      color:[null, Validators.required],
+      transmission:[null, Validators.required],
+      price:[null, Validators.required],
+      description:[null, Validators.required],
+      yaer:[null, Validators.required]
+    })
+  }
+
+  postCar(){
+    console.log(this.postCarForm.value);
+    const formData: FormData = new FormData();
+    formData.append('img', this.selectedFile);
+    formData.append('brand', this.postCarForm.get('brand')?.value);
+    formData.append('name', this.postCarForm.get('name')?.value);
+    formData.append('type', this.postCarForm.get('type')?.value);
+    formData.append('color', this.postCarForm.get('color')?.value);
+    formData.append('year', this.postCarForm.get('year')?.value);
+    formData.append('transmission', this.postCarForm.get('transmission')?.value);
+    formData.append('description', this.postCarForm.get('description')?.value);
+    formData.append('price', this.postCarForm.get('price')?.value);
+    console.log(formData);
+    }
+
+  onFileSelected(event: any){
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+  }
+
+  previewImage(){
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
+  }
 
 }
