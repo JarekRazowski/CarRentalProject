6c9c42932b978953e487d5ef85f250a40182e71b
Jarosław Razowski <j.razowski19@onet.pl>
2024-01-31 18:49:16 +0100

    1

--- src/main/java/com/jaroslaw/carrentalproject/CarRentalProjectApplication.java
+++ src/main/java/com/jaroslaw/carrentalproject/CarRentalProjectApplication.java
@@ -1,11 +1,15 @@
 package com.jaroslaw.carrentalproject;
 
+import com.jaroslaw.carrentalproject.entity.Car;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.jaroslaw.carrentalproject.dto.CarDto;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class CarRentalProjectApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(CarRentalProjectApplication.class, args);}
+        SpringApplication.run(CarRentalProjectApplication.class, args);
+    }
 }
