7795625b039b0285107bf6a2219c96a2385dbec3
Jarosław Razowski <j.razowski19@onet.pl>
2024-02-01 16:05:42 +0100

    1

--- src/main/java/com/jaroslaw/carrentalproject/CarRentalProjectApplication.java
+++ src/main/java/com/jaroslaw/carrentalproject/CarRentalProjectApplication.java
@@ -1,11 +1,7 @@
 package com.jaroslaw.carrentalproject;
 
-import com.jaroslaw.carrentalproject.entity.Car;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.jaroslaw.carrentalproject.dto.CarDto;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class CarRentalProjectApplication {
--- src/main/java/com/jaroslaw/carrentalproject/entity/Car.java
+++ src/main/java/com/jaroslaw/carrentalproject/entity/Car.java
@@ -11,16 +11,25 @@ import java.util.Date;
 public class Car {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id", nullable = false)
     private Long id;
+    @Column(name = "Car_brand", nullable = false)
     private String brand;
+    @Column(name = "Car_color", nullable = false)
     private String color;
+    @Column(name = "Car_name", nullable = false)
     private String name;
+    @Column(name = "Car_type", nullable = false)
     private String type;
+    @Column(name = "Car_transmission", nullable = false)
     private String transmission;
+    @Column(name = "Car_description", nullable = false)
     private String description;
+    @Column(name = "Car_price", nullable = false)
     private Long price;
+    @Column(name = "Car_year", nullable = false)
     private Date year;
-    @Column(columnDefinition = "longlob")
+    @Column(columnDefinition = "longblob")
     private byte[] image;
 }
