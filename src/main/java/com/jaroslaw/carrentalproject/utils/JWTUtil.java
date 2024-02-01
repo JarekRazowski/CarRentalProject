691e3af2ebfd5e0af56d217a9eb37424e7a9ffac
Jarosław Razowski <j.razowski19@onet.pl>
2024-02-01 19:39:56 +0100

    1

--- src/main/java/com/jaroslaw/carrentalproject/utils/JWTUtil.java
+++ src/main/java/com/jaroslaw/carrentalproject/utils/JWTUtil.java
@@ -38,7 +38,7 @@ public class JWTUtil {
     private String generateToken(Map<String, Object> extractClaims, UserDetails userDetails){
         return Jwts.builder().setClaims(extractClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
     }
 
